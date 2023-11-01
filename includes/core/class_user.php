9fb1279096b6c566d626681de8965db13f0325ee
Stas Kushnir <stas.kushnir@gmail.com>
2023-11-01 20:08:00 +0300

    add method users_list() in class User

--- includes/core/class_user.php
+++ includes/core/class_user.php
@@ -1,19 +1,24 @@
 <?php
 
-class User {
-
+class User
+{
     // GENERAL
 
-    public static function user_info($d) {
+    public static function user_info($d)
+    {
         // vars
         $user_id = isset($d['user_id']) && is_numeric($d['user_id']) ? $d['user_id'] : 0;
         $phone = isset($d['phone']) ? preg_replace('~\D+~', '', $d['phone']) : 0;
         // where
-        if ($user_id) $where = "user_id='".$user_id."'";
-        else if ($phone) $where = "phone='".$phone."'";
-        else return [];
+        if ($user_id) {
+            $where = "user_id='" . $user_id . "'";
+        } elseif ($phone) {
+            $where = "phone='" . $phone . "'";
+        } else {
+            return [];
+        }
         // info
-        $q = DB::query("SELECT user_id, phone, access FROM users WHERE ".$where." LIMIT 1;") or die (DB::error());
+        $q = DB::query('SELECT user_id, phone, access FROM users WHERE ' . $where . ' LIMIT 1;') or die(DB::error());
         if ($row = DB::fetch_row($q)) {
             return [
                 'id' => (int) $row['user_id'],
@@ -27,25 +32,69 @@ class User {
         }
     }
 
-    public static function users_list_plots($number) {
+    public static function users_list_plots($number)
+    {
         // vars
         $items = [];
         // info
         $q = DB::query("SELECT user_id, plot_id, first_name, email, phone
-            FROM users WHERE plot_id LIKE '%".$number."%' ORDER BY user_id;") or die (DB::error());
+            FROM users WHERE plot_id LIKE '%" . $number . "%' ORDER BY user_id;") or die(DB::error());
         while ($row = DB::fetch_row($q)) {
             $plot_ids = explode(',', $row['plot_id']);
             $val = false;
-            foreach($plot_ids as $plot_id) if ($plot_id == $number) $val = true;
-            if ($val) $items[] = [
-                'id' => (int) $row['user_id'],
-                'first_name' => $row['first_name'],
-                'email' => $row['email'],
-                'phone_str' => phone_formatting($row['phone'])
-            ];
+            foreach ($plot_ids as $plot_id) {
+                if ($plot_id == $number) {
+                    $val = true;
+                }
+            }
+            if ($val) {
+                $items[] = [
+                    'id' => (int) $row['user_id'],
+                    'first_name' => $row['first_name'],
+                    'email' => $row['email'],
+                    'phone_str' => phone_formatting($row['phone'])
+                ];
+            }
         }
         // output
         return $items;
     }
 
+    public static function users_list($d = [])
+    {
+        // vars
+        $search = isset($d['search']) && trim($d['search']) ? $d['search'] : '';
+        $offset = isset($d['offset']) && is_numeric($d['offset']) ? $d['offset'] : 0;
+        $limit = 20;
+        $items = [];
+        // where
+        $where = [];
+        if ($search) {
+            $where[] = "LIKE '%" . $search . "%'";
+        }
+        $where = $where ? 'WHERE ' . implode(' AND ', $where) : '';
+        // info
+        $q = DB::query('SELECT plot_id, first_name, last_name, phone, email, last_login
+            FROM users ' . $where . ' ORDER BY user_id LIMIT ' . $offset . ', ' . $limit . ';') or die(DB::error());
+        while ($row = DB::fetch_row($q)) {
+            $items[] = [
+                'id' => (int) $row['plot_id'],
+                'first_name' => $row['first_name'],
+                'last_name' => $row['last_name'],
+                'phone' => $row['phone'],
+                'email' => $row['email'],
+                'last_login' => date('Y/m/d', $row['last_login'])
+            ];
+        }
+        // paginator
+        $q = DB::query('SELECT count(*) FROM users ' . $where . ';');
+        $count = ($row = DB::fetch_row($q)) ? $row['count(*)'] : 0;
+        $url = 'users?';
+        if ($search) {
+            $url .= '&search=' . $search;
+        }
+        paginator($count, $offset, $limit, $url, $paginator);
+        // output
+        return ['items' => $items, 'paginator' => $paginator];
+    }
 }
