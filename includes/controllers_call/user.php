3361aa0c4527f2a428036c3a9cbb8abc8a008032
Stas Kushnir <stas.kushnir@gmail.com>
2023-11-02 18:58:34 +0300

    fix conroller user

--- includes/controllers_call/user.php
+++ includes/controllers_call/user.php
@@ -3,10 +3,10 @@
 function controller_user($act, $d)
 {
     if ($act == 'edit_window') {
-        return User::plot_edit_window($d);
+        return User::user_edit_window($d);
     }
     if ($act == 'edit_update') {
-        return User::plot_edit_update($d);
+        return User::user_edit_update($d);
     }
     return '';
 }
