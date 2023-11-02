fd7445cd6a8991a61596b18a6b640cf759f91401
Stas Kushnir <stas.kushnir@gmail.com>
2023-11-02 20:17:06 +0300

    fix call.php

--- call.php
+++ call.php
@@ -2,10 +2,12 @@
 
 // INIT
 
-if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') echo '';
+if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    echo '';
+}
 
-require('./cfg/general.inc.php');
-require('./includes/core/functions.php');
+require './cfg/general.inc.php';
+require './includes/core/functions.php';
 
 init_classes();
 init_controllers_call();
@@ -14,11 +16,11 @@ DB::connect();
 
 // VARS
 
-$location = isset($_POST['location']) ? flt_input($_POST['location']) : NULL;
-$data = isset($_POST['data']) ? flt_input($_POST['data']) : NULL;
+$location = isset($_POST['location']) ? flt_input($_POST['location']) : null;
+$data = isset($_POST['data']) ? flt_input($_POST['data']) : null;
 
-$dpt = $location['dpt'] ?? NULL;
-$act = $location['act'] ?? NULL;
+$dpt = $location['dpt'] ?? null;
+$act = $location['act'] ?? null;
 
 // SESSION
 
